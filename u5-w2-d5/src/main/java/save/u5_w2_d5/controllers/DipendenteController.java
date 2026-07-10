777c6096cecdab2f02e5f629c77dd85217eb3f75
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-07-10 11:55:59 +0200

    add GET

--- u5-w2-d5/src/main/java/save/u5_w2_d5/controllers/DipendenteController.java
+++ u5-w2-d5/src/main/java/save/u5_w2_d5/controllers/DipendenteController.java
@@ -2,10 +2,13 @@ package save.u5_w2_d5.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import save.u5_w2_d5.entities.Dipendente;
 import save.u5_w2_d5.payloads.DipendentePayload;
 import save.u5_w2_d5.services.DipendenteService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/dipendenti")
@@ -16,5 +19,26 @@ public class DipendenteController {
     @PostMapping
     public Dipendente save(@RequestBody @Valid DipendentePayload body) {
         return dipendenteService.save(body);
+    }
+
+    @GetMapping
+    public List<Dipendente> findAll() {
+        return dipendenteService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Dipendente findById(@PathVariable long id) {
+        return dipendenteService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Dipendente update(@PathVariable long id, @RequestBody @Valid DipendentePayload body) {
+        return dipendenteService.update(id, body);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204: Operazione avvenuta con successo senza corpo di risposta
+    public void delete(@PathVariable long id) {
+        dipendenteService.findByIdAndDelete(id);
     }
 }
--- u5-w2-d5/src/main/java/save/u5_w2_d5/services/DipendenteService.java
+++ u5-w2-d5/src/main/java/save/u5_w2_d5/services/DipendenteService.java
@@ -6,6 +6,8 @@ import save.u5_w2_d5.entities.Dipendente;
 import save.u5_w2_d5.payloads.DipendentePayload;
 import save.u5_w2_d5.repositories.DipendenteRepository;
 
+import java.util.List;
+
 @Service
 public class DipendenteService {
     @Autowired
@@ -18,5 +20,29 @@ public class DipendenteService {
         d.setCognome(body.cognome());
         d.setEmail(body.email());
         return dr.save(d);
+    }
+
+    public List<Dipendente> findAll() {
+        return dr.findAll();
+    }
+
+    public Dipendente findById(long id) {
+        return dr.findById(id).orElseThrow(() -> new RuntimeException("Dipendente con id " + id + " non trovato"));
+    }
+
+    // UPDATE
+    public Dipendente update(long id, DipendentePayload body) {
+        Dipendente d = findById(id);
+        d.setUsername(body.username());
+        d.setNome(body.nome());
+        d.setCognome(body.cognome());
+        d.setEmail(body.email());
+        return dr.save(d);
+    }
+
+    // DELETE
+    public void findByIdAndDelete(long id) {
+        Dipendente found = findById(id);
+        dr.delete(found);
     }
 }
